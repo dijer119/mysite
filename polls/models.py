b2771c0a98215f1826496427c011fb637120f30d
dijer park <dijer119@gmail.com>
2015-02-16 14:09:05 +0900

    life log add

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 import datetime
 
 # Create your models here.
-# Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
