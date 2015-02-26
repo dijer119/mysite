197e8ac6f0e796098a74d6737b3516092c70ebe2
dijer park <dijer119@gmail.com>
2015-02-27 08:39:37 +0900

    rango add

--- lifelog/models.py
+++ lifelog/models.py
@@ -1,3 +1,34 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
+
+# class User(models.Model):
+#     create_date = models.DateTimeField('Create Date')
+#     update_date = models.DateTimeField('Update Date')
+#
+#     #email 고려
+#     user_id = models.CharField(max_length=200)
+#
+# class Log(models.Model):
+#     create_date = models.DateTimeField('Create Date')
+#     update_date = models.DateTimeField('Update Date')
+#
+#     #실제 log 발생한 일시
+#     log_date = models.DateTimeField('Event Date')
+#
+#     #log type, 약, 운동, 먹는거 등등.
+#     type = models.ForeignKey(Code)
+#
+#     #하위 코드명 이나 간단하게 등록.
+#     text = models.CharField(max_length=200)
+#
+#     #
+#
+#
+#     user = models.ForeignKey(User)
+#
+# class Code(models.Model):
+#     code = models.CharField(max_length=10)
+#     code_name = models.CharField(max_length=50)
+#     p_code = models.CharField(max_length=10)
