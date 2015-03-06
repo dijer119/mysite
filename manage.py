120a442b195fa94efbcba133c77e343ebe0cfa2e
dijer park <dijer119@gmail.com>
2015-03-07 08:57:05 +0900

    category 추가 및 slug 추가.

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+   # os.environ["LANG"]="ko_KR.UTF-8"
 
     from django.core.management import execute_from_command_line
 
