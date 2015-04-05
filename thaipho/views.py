bc4d6e502078d59a42fe92a7f20ad0276d7cd68c
dijer119 <dijer119@gmail.com>
2015-04-06 06:54:02 +0900

    thaipho 추가.

--- thaipho/views.py
+++ thaipho/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 # Create your views here.
 def index(request):
+    return render(request, 'thaipho2/index.html')
+
+def index2(request):
     return render(request, 'thaipho/index.html')
